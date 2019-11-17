e27f283a8ecf1a00132f5533ab7e045f093034a2
Artem Golovin <artgolovin96@gmail.com>
2019-11-16 21:58:09 -0700

    feat(ios): add method stubs

--- ios/Classes/SwiftFflashlightPlugin.swift
+++ ios/Classes/SwiftFflashlightPlugin.swift
@@ -1,14 +1,44 @@
 import Flutter
 import UIKit
+import AVFoundation
 
 public class SwiftFflashlightPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "fflashlight", binaryMessenger: registrar.messenger())
-    let instance = SwiftFflashlightPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "fflashlight", binaryMessenger: registrar.messenger())
+        let instance = SwiftFflashlightPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    var hasFlashlight: Bool {
+        get {
+            guard let device = AVCaptureDevice.default(for: .video) else { return false }
+            return device.hasTorch || device.hasFlash
+        }
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "enable":
+            guard let args = call.arguments as? [String: Any] else { return }
+            guard let state = args["state"] as? Bool else { return }
+            result(enable(state: state))
+            break
+        case "on":
+            result(enable(state: true))
+            break
+        case "off":
+            result(enable(state: false))
+            break
+        case "hasFlashlight":
+            result(hasFlashlight)
+            break
+        default:
+            result(FlutterMethodNotImplemented)
+            break
+        }
+    }
+
+    private func enable(state: Bool) {
+        
+    }
 }
